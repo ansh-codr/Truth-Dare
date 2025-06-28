a51ecd24caa08e12729207f6a3d2c5cd0b9f1e2a
Noctovok <anshloverao@gmail.com>
2025-06-28 12:37:00 +0530

    Fix Netlify build configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -12,19 +12,6 @@ const nextConfig = {
   },
   trailingSlash: true,
   distDir: 'out',
-  assetPrefix: '',
-  basePath: '',
-  swcMinify: true,
-  experimental: {
-    optimizeCss: true,
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  // Generate a consistent build ID
-  generateBuildId: async () => {
-    return 'truth-dare-game-build'
-  },
 };
 
 module.exports = nextConfig;
